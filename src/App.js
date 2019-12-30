2572348406019d4b21412b0ffd8f10db80712b00
Wynell <wynell@Wynells-iMac.local>
2019-12-30 22:39:20 +0300

    Hello, world!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { View, Panel, PanelHeader, Group, List, Cell, Div, Root, CellButton } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeView: 'view1'
+    }
+  }
+
+  render() {
+    return (
+      <Root activeView={this.state.activeView}>
+        <View activePanel="panel1.1" id="view1">
+          <Panel id="panel1.1">
+            <PanelHeader>Главный экран</PanelHeader>
+            <Group>
+              <CellButton onClick={() => this.setState({ activeView: 'view2' })}>
+                Open View 2
+              </CellButton>
+            </Group>
+          </Panel>
+        </View>
+        <View activePanel="panel2.1" id="view2">
+          <Panel id="panel2.1">
+            <PanelHeader>View 2</PanelHeader>
+            <Group>
+              <CellButton onClick={() => this.setState({ activeView: 'view1' })}>
+                Back to View 1
+              </CellButton>
+            </Group>
+          </Panel>
+        </View>
+      </Root>
+    )
+  }
 }
 
 export default App;
